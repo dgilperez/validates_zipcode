17ee039fd89adc01382c260248b665af959d7b42
David Gil <dgilperez@gmail.com>
2018-02-13 20:10:07 +0100

    :sparkles: adds excluded_country_codes option to be able to configure the validator and avoid some specific country codes

--- lib/validates_zipcode/validator.rb
+++ lib/validates_zipcode/validator.rb
@@ -17,17 +17,23 @@ require 'active_model/validator'
 module ValidatesZipcode
   class Validator < ActiveModel::EachValidator
     def initialize(options)
-      @country_code           = options.fetch(:country_code) { }
-      @country_code_attribute = options.fetch(:country_code_attribute) { :country_alpha2 }
+      @country_code           = options.fetch(:country_code, nil)
+      @country_code_attribute = options.fetch(:country_code_attribute, :country_alpha2)
+      @excluded_country_codes = options.fetch(:excluded_country_codes, [])
 
       super
     end
 
     def validate_each(record, attribute, value)
-      alpha2 = @country_code || record.send(@country_code_attribute)
+      alpha2  = @country_code || record.send(@country_code_attribute)
+      options = { zipcode: value.to_s,
+                  country_alpha2: alpha2,
+                  excluded_country_codes: @excluded_country_codes }
 
-      unless ValidatesZipcode::Zipcode.new(zipcode: value.to_s, country_alpha2: alpha2).valid?
-        record.errors.add(attribute, :invalid_zipcode, message: I18n.t('errors.messages.invalid_zipcode', value: value, default: 'Zipcode is invalid'))
+      unless ValidatesZipcode::Zipcode.new(options).valid?
+        record.errors.add(attribute, :invalid_zipcode, message: I18n.t('errors.messages.invalid_zipcode',
+                                                       value: value,
+                                                       default: 'Zipcode is invalid'))
       end
     end
   end
